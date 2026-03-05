5a1e0aa0ec78176e13c67bebd2075200f5f77fa9
Matheus Araujo <matheus.208@hotmail.com>
2026-03-05 11:23:12 -0300

    enviando projeto3

---
+++ novo 5.js
@@ -0,0 +1,42 @@
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo.toLowerCase(); // Garante que o tipo fique em letras minúsculas
+  }
+
+  atacar() {
+    let ataque = "";
+
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "um ataque desconhecido";
+    }
+
+    console.log(`o ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const herois = [
+  new Heroi("Gandalf", 2000, "Mago"),
+  new Heroi("Conan", 35, "Guerreiro"),
+  new Heroi("Aang", 112, "Monge"),
+  new Heroi("Naruto", 17, "Ninja")
+];
+
+console.log("--- Iniciando os ataques ---");
+for (const heroi of herois) {
+  heroi.atacar();
+}
